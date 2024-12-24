c1c0d2a4ca8326f75bfe95c436d17cecd5c371a2
kai5041 <kai5041contact@gmail.com>
2024-12-24 16:27:54 +0100

    Backup

--- src/main.cpp
+++ src/main.cpp
@@ -3,42 +3,38 @@
 struct Command {
   std::string name;
   std::string description;
-  std::string help;
+  std::string syntax;
 };
 
 using Commands = std::vector<Command>;
 Commands commands = {
-    {"help", "Prints this help", "prm help"},
-    {"version", "Prints the version", "prm version"},
-    {"run", "Runs the program", "prm run"},
+    {"help", "Prints this message", "prm help"},
+    {"version", "Prints current prm version", "prm version"},
+    {"run", "Run the project", "prm run <project_name>"},
+    {"init", "Initialize a new project", "prm init"},
+    {"create", "Create a new prm project"}
 };
 
 int print_help(Commands &commands) {
-  int longest_name = 0;
-  int longest_description = 0;
-  int longest_help = 0;
+  int longest_name = 0, longest_description = 0, longest_syntax = 0;
   for (auto &command : commands) {
-    if (command.name.size() > longest_name) {
-      longest_name = command.name.size();
-    }
-    if (command.description.size() > longest_description) {
-      longest_description = command.description.size();
-    }
-    if (command.help.size() > longest_help) {
-      longest_help = command.help.size();
-    }
+    longest_name = std::max(longest_name, (int)command.name.size());
+    longest_description =
+        std::max(longest_description, (int)command.description.size());
+    longest_syntax = std::max(longest_syntax, (int)command.syntax.size());
   }
   longest_name += 2;
   longest_description += 2;
-  longest_help += 2;
+  longest_syntax += 2;
 
-  cout << "Command" << std::string(longest_name - 7, ' ') << "Description\n";
-
+  cout << "Command" << std::string(longest_name - 7, ' ') << "Description"
+       << std::string(longest_description - 11, ' ') << "Syntax\n";
   for (auto &command : commands) {
     cout << command.name << std::string(longest_name - command.name.size(), ' ')
-         << command.description << "\n";
+         << command.description
+         << std::string(longest_description - command.description.size(), ' ')
+         << command.syntax << "\n";
   }
-
   return 0;
 }
 
